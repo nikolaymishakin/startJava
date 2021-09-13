29923c4ef27bd51388c701f2c77e23c37beecf9b
Николай <nikolaymishakin@yandex.ru>
2021-09-13 13:31:16 +0300

    Implements HW_2

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -2,38 +2,40 @@ import java.util.Scanner;
 import static java.lang.Math.*;
 
 public class ConditionalStatement {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.io.IOException {
         Scanner scanner = new Scanner(System.in); 
         System.out.println("Введите Ваш возраст:");
         int age = scanner.nextInt();
         if (age > 20) {
             System.out.println("Вам больше 20");
         }
+
         System.out.println("Введите М если Вы мужчина или Ж если женщина:");
         char gender = scanner.next().charAt(0);
-        boolean isgender = gender == 'M';
-        if (isgender) {
+        boolean isGender = gender == 'M';
+        if (isGender) {
             System.out.println("Вы мужчина");
         }
-        if (!isgender) {
+        if (!isGender) {
             System.out.println("Вы женщина");
         }
+
         System.out.println("Введите Ваш рост:");
         double height = scanner.nextDouble();
-        boolean isTrue = Math.abs(height - 1.80) <= 0.00001;
-        if (isTrue) {
+        if (height <= 1.8) {
             System.out.println("Ваш рост меньше 1 метра 80 сантиметров");
         } else {
             System.out.println("Ваш рост больше 1 метра 80 сантиметров");
         }
+
         System.out.println("Введите первую букву Вашего имени:");
-        char name = scanner.next().charAt(0);
-        if (name == 'M') {
+            char name = (char) System.in.read (); 
+            if (name == 'M') {
             System.out.println("Вас могут звать Mikle");
-        } else if (name == 'I') {
+            } else if (name == 'I') {
             System.out.println("Вас могут звать Ilya");
-        } else {
+            } else {
             System.out.println("Вас могут звать как угодно");
-        }
+            }
     }
 }
--- Variable.java
+++ Variable.java
@@ -1,20 +1,20 @@
 public class Variable {
     public static void main(String[] args) {
-    boolean multimonitor = false;
-    byte memory = 4;
-    short ssd = 512;
-    char m = 'm';
-    int nuclear = 2;
-    long distance = 147_600_000_000L;
-    float gHz = 1.6f;
-    double size = 512.00;
-    System.out.println("Наличие двух мониторов: " + multimonitor);
-    System.out.println("Оперативная память: " + memory + " Гб");
-    System.out.println("Память SSD: " + ssd + " Гб");
-    System.out.println("Символ : " + m);
-    System.out.println("Количество ядер: " + nuclear);
-    System.out.println("Расстояние от Земли до Солнца: " + distance + " метров");
-    System.out.println("Частота ядер: " + gHz + " GHz");
-    System.out.println("Размер жёсткого диска: " + size + " Gb");
+        boolean multimonitor = false;
+        byte memory = 4;
+        short ssd = 512;
+        char m = 'm';
+        int cores = 2;
+        long distance = 147_600_000_000L;
+        float gHz = 1.6f;
+        double size = 512.00;
+        System.out.println("Наличие двух мониторов: " + multimonitor);
+        System.out.println("Оперативная память: " + memory + " Гб");
+        System.out.println("Память SSD: " + ssd + " Гб");
+        System.out.println("Символ : " + m);
+        System.out.println("Количество ядер: " + cores);
+        System.out.println("Расстояние от Земли до Солнца: " + distance + " метров");
+        System.out.println("Частота ядер: " + gHz + " GHz");
+        System.out.println("Размер жёсткого диска: " + size + " Gb");
     }
 }
